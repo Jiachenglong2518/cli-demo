3de99c06dadd77ccb26d41dbbfde2d29c21ff191
jiahcenglong <jiachenglong@jihai8.com>
2020-09-03 17:46:40 +0800

    组件

---
+++ src/components/leftmenu.vue
@@ -0,0 +1,23 @@
+<template>
+  <section>
+    <ul class="left-menu">
+      <li>主页</li>
+      <li>我的账户</li>
+      <li>历时记录</li>
+    </ul>
+  </section>
+</template>
+<style lang="css" scoped>
+*{
+  margin: 0;
+  padding: 0;
+}
+.left-menu{
+  width: 200px;
+}
+.left-menu li{
+  margin-top: 5px;
+  list-style: none;
+  background :green;
+}
+</style>
---
+++ src/views/personal/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <section>
+    <h3>欢迎来到个人中心</h3>
+    <left-menu></left-menu>
+    <!-- <router-view></router-view> -->
+  </section>
+</template>
+<script>
+  import leftMenu from '../../components/leftmenu'
+  export default {
+    components : {
+      'left-menu' : leftMenu
+    }
+  }
+</script>
